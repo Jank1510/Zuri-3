5ba95a4faad888feae774b1e46b45078fa7c1c06
Jank1510 <89560208+Jank1510@users.noreply.github.com>
2022-06-20 16:48:10 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+
+    swal("Sabia que dirias que si soy muy sexy","si quieres te dejo hacerme un hijo 7w7")
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
+noBtn.addEventListener('click', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
